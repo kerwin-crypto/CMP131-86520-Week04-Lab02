c42f864af217fc92a599fc2575394cbf9c8d345a
kerwin-crypto <aguilar.kerwin@student.ccm.edu>
2026-09-23 12:00:53 -0400

    Completed

---
+++ arithmetic_operations.py
@@ -0,0 +1,20 @@
+#Kerwin Aguilar
+#CMP131-86520
+#Week04
+#Lab02
+#Arithmetic Operations
+#09/23/2026
+num1=int(input('Enter Your First Number:'))
+num2=int(input('Enter Your Second Number:'))
+add=num1+num2
+sub=num1-num2
+mult=num1*num2
+divi=num1/num2
+power=num1**num2
+avg=(num1+num2)/2
+print('Addition:',add)
+print('Subtraction:',sub)
+print('Multiplication:',mult)
+print('Division',divi)
+print("Power:",power)
+print('Average:',avg)
